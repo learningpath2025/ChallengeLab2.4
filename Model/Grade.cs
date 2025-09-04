fea84791d84e710e6b19c3d188c608b3d05d4294
learningpath2025 <learningpath2025@outlook.com>
2025-09-04 10:05:44 -0500

    adding grade class

---
+++ Model/Grade.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeLab2._4.Model
+{
+    internal class Grade
+    {
+        public int GradeId { get; set; }
+        public int StudentId { get; set; }
+        public required string Grades { get; set; }
+
+    }
+}
